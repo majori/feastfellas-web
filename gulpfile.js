e4a9ce81a7cffe3b2d4a23144bfd0fcd0508fb5f
Antti Kivimäki <majoriz11@gmail.com>
2016-08-07 22:27:42 +0300

    [WIP] Working on navigation
    Former-commit-id: 6cf9ca82681d41fa6e0d6a1050aa0ab9023626db [formerly 4c7424a46570dac1e27a42242316f56f20bba703]
    Former-commit-id: 57af741bce2c2bc19267f4d546a636e7edb84f61

--- gulpfile.js
+++ gulpfile.js
@@ -10,19 +10,18 @@ const autoprefixer  = require('gulp-autoprefixer');
 const SERVER_ADDRESS = 'localhost';
 const SERVER_PORT = 8080;
 
-const TEMPLATE_DIR = path.resolve(__dirname, 'src/templates');
-const STYLE_DIR = path.resolve(__dirname, 'src/styles');
+const SOURCE_DIR = path.resolve(__dirname, 'src');
 const PUBLIC_DIR = path.resolve(__dirname, 'public');
 
 // External style libraries
-const BOOTSTRAP_DIR = path.resolve(__dirname, 'node_modules/bootstrap-sass/assets/stylesheets');
 const VIEWPORTS_DIR = path.resolve(__dirname, 'node_modules/viewports');
+const PURECSS_DIR = path.resolve(__dirname, 'node_modules/purecss-sass/vendor/assets/stylesheets')
 
 const production = !!util.env.production; // Turn undefined into a proper false
 
 const CONFIG = {
   sass: {
-    includePaths: [ STYLE_DIR, BOOTSTRAP_DIR, VIEWPORTS_DIR],
+    includePaths: [ SOURCE_DIR, VIEWPORTS_DIR, PURECSS_DIR],
     outputStyle: (production) ? 'compressed' : 'nested'
   },
 
@@ -40,13 +39,13 @@ const CONFIG = {
 }
 
 gulp.task('build_views', () => {
-  return gulp.src(`${TEMPLATE_DIR}/views/*.pug`)
+  return gulp.src(`${SOURCE_DIR}/views/*.pug`)
     .pipe(pug(CONFIG.pug))
     .pipe(gulp.dest(`${PUBLIC_DIR}/views`));
 });
 
 gulp.task('build_styles', () => {
-  return gulp.src(`${STYLE_DIR}/**/*.scss`)
+  return gulp.src(`${SOURCE_DIR}/styles/*.scss`)
     .pipe(sass(CONFIG.sass).on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
@@ -56,8 +55,8 @@ gulp.task('build_styles', () => {
 });
 
 gulp.task('watch', () => {
-  gulp.watch(`${TEMPLATE_DIR}/**/*.pug`, ['build_views']);
-  gulp.watch(`${STYLE_DIR}/**/*.scss`, ['build_styles']);
+  gulp.watch(`${SOURCE_DIR}/**/*.pug`, ['build_views']);
+  gulp.watch(`${SOURCE_DIR}/**/*.scss`, ['build_styles']);
 });
 
 gulp.task('webserver', () => {
